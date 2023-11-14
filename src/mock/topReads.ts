5895306c76a200d598d593459b7560e41809f4d9
nano235 <revivaladolor@yahoo.com>
2023-11-14 23:52:23 +0100

    updates

---
+++ src/mock/topReads.ts
@@ -0,0 +1,28 @@
+export interface TopRead {
+	href?: string;
+	title?: string;
+	imgSrc?: string;
+}
+
+export const topReads: TopRead[] = [
+	{
+		href: "#",
+		title: "What Are the Signs and Symptoms of Depression in Men?",
+		imgSrc: "https://media.post.rvohealth.io/wp-content/uploads/2021/02/man-listening-to-friend-in-cafe-732x549-thumbnail.jpg",
+	},
+	{
+		href: "#",
+		title: "What Tea Should You Drink to Lower Blood Pressure?",
+		imgSrc: "https://media.post.rvohealth.io/wp-content/uploads/2023/08/3140612-What-Tea-Should-You-Drink-to-Lower-Blood-Pressure-732x549-thumbnail.jpg",
+	},
+	{
+		href: "#",
+		title: "Understanding Cancer Dizziness and Fatigue",
+		imgSrc: "https://media.post.rvohealth.io/wp-content/uploads/2023/08/Bald-man-with-eye-patches-drinking-coffee-thumbnail.jpg",
+	},
+	{
+		href: "#",
+		title: "How Menopause Encouraged Me to Give Up Dieting",
+		imgSrc: "https://media.post.rvohealth.io/wp-content/uploads/2023/08/female-making-food-in-kitchen-732-549-feature-thumb.jpg",
+	},
+];
