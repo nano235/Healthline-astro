251a55f3887a5986cb4233c7d68501ed2143f25a
nano235 <revivaladolor@yahoo.com>
2023-11-18 20:40:01 +0100

    updates

---
+++ src/mock/navLinks.ts
@@ -0,0 +1,69 @@
+export const navLinks = [
+	{
+		title: "health conditions",
+		url: "/",
+		id: 1,
+		subMenu: [
+			{
+				title: "featured",
+				menus: [
+					{ title: "diabetes", url: "#" },
+					{ title: "heart disease", url: "#" },
+					{ title: "mental health", url: "#" },
+				],
+			},
+			{
+				title: "articles",
+				menus: [
+					{ title: "latest news", url: "#" },
+					{ title: "news archive", url: "#" },
+				],
+			},
+		],
+	},
+	{
+		title: "discover",
+		url: "/",
+		id: 2,
+		subMenu: [
+			{
+				title: "wellness topics",
+				menus: [
+					{ title: "anxiety", url: "#" },
+					{ title: "depression", url: "#" },
+					{ title: "digestive issues", url: "#" },
+				],
+			},
+			{
+				title: "product reviews",
+				menus: [
+					{ title: "medications", url: "#" },
+					{ title: "supplements", url: "#" },
+				],
+			},
+		],
+	},
+	{
+		title: "plan",
+		url: "/",
+		id: 3,
+		subMenu: [
+			{
+				title: "health challenges",
+				menus: [
+					{ title: "weight loss", url: "#" },
+					{ title: "fitness goals", url: "#" },
+					{ title: "nutrition plans", url: "#" },
+				],
+			},
+			{
+				title: "find care",
+				menus: [
+					{ title: "doctors", url: "#" },
+					{ title: "therapists", url: "#" },
+					{ title: "coaches", url: "#" },
+				],
+			},
+		],
+	},
+];
